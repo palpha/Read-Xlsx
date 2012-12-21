4d43e95f2a727ad40b379298c4ae59304a53fd67
Niklas Bergius <niklas@bergius.org>
2012-12-21 12:37:12 +0100

    Better sheet selection, new function Get-XlsxInfo.

--- Read-Xlsx.psm1
+++ Read-Xlsx.psm1
@@ -23,30 +23,66 @@ function Read-Xlsx {
             ValueFromPipelineByPropertyName)]
         [Alias('FullName')]
         [System.IO.FileInfo[]] $FilePath,
-        [string] $SheetName = 'Sheet1'
+        $Sheet = 0 # name or idx
     )
 
     process {
         $FilePath | ? { Is-Xslx $_ } | % {
-            Write-Host $_
-            $reader = Create-Reader $_.FullName
-            Write-Host $reader
-            $colNames = @()
-            $reader.ReadSheet($SheetName) | % {
-                $row = $_
-                if ($_.Get('A').Row -eq 1) {
-                    $row | % { $colNames += @{ Column = $_.Column; Name = $_.Value } }
-                    $null
-                } else {
-                    $rowConverted = @{}
-                    $colNames | % {
-                        $rowConverted.($_.Name) = $row.Get($_.Column)
+            try {
+                $reader = Create-Reader $_.FullName
+                $colNames = @()
+                $reader.ReadSheet($Sheet) | % {
+                    $row = $_
+                    if ($_.Get('A').Row -eq 1) {
+                        $row | % { $colNames += @{ Column = $_.Column; Name = $_.Value } }
+                        $null
+                    } else {
+                        $rowConverted = @{}
+                        $colNames | % {
+                            $rowConverted.($_.Name) = $row.Get($_.Column)
+                        }
+                        [PSCustomObject] $rowConverted
                     }
-                    [PSCustomObject] $rowConverted
+                } | ? { $_ -ne $null }
+            } finally {
+                $reader.Dispose()
+            }
+        }
+    }
+}
+
+function Get-XlsxInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [Alias('FullName')]
+        [System.IO.FileInfo[]] $FilePath
+    )
+
+    process {
+        $FilePath | ? { Is-Xslx $_ } | % {
+            try {
+                $reader = Create-Reader $_.FullName
+                [PSCustomObject] @{
+                    FilePath = $_.FullName
+                    Sheets = $reader.Sheets | % {
+                        [PSCustomObject] @{
+                            Name = $_.Name
+                            Columns = $reader.ReadSheet($_.Name) | select -First 1 | % {
+                                $_ | % { $_.Value }
+                            }
+                        }
+                    }
                 }
-            } | ? { $_ -ne $null }
+            } finally {
+                $reader.Dispose()
+            }
         }
     }
 }
 
 Export-ModuleMember -Function Read-Xlsx
+Export-ModuleMember -Function Get-XlsxInfo
